e88e5a3065a1ed9e399dcb09d918638e52afe538
kesar <claymantwinkle@gmail.com>
2022-08-13 18:09:16 +0800

    opt: 剑气增加释放冷却时间

--- src/main/java/com/flysword/network/server/SpawnSwordBeamPacket.java
+++ src/main/java/com/flysword/network/server/SpawnSwordBeamPacket.java
@@ -36,23 +36,27 @@ public class SpawnSwordBeamPacket implements IMessage, IMessageHandler<SpawnSwor
             ItemStack stack = player.getHeldItemMainhand();
 
             int level;
-            if( (level = EnchantmentHelper.getEnchantmentLevel(ModEnchantments.sSwordBeam, stack)) > 0) {
-                stack.damageItem(new Random().nextBoolean()?1:0, player);
+            if ((level = EnchantmentHelper.getEnchantmentLevel(ModEnchantments.sSwordBeam, stack)) > 0 && player.getCooldownTracker().getCooldown(stack.getItem(), 0) == 0) {
+                stack.damageItem(new Random().nextBoolean() ? 1 : 0, player);
                 PlayerUtils.playSoundAtEntity(player.world, player, SoundEvents.ENTITY_PLAYER_ATTACK_SWEEP, SoundCategory.PLAYERS, 0.4F, 0.5F);
                 float damage = getDamage(player, stack) * (level * 0.25f);
                 EntitySwordBeam beam = new EntitySwordBeam(player.world, player).setLevel(level).setDamage(damage);
                 beam.shoot(player, player.rotationPitch, player.rotationYaw, 0.0F, beam.getVelocity(), 1.0F);
                 player.world.spawnEntity(beam);
+
+                // TODO 可配置
+                player.getCooldownTracker().setCooldown(stack.getItem(), 80 - level * 13);
             }
         }
         return null;
     }
 
-    /** Returns player's base damage (with sword) plus 1.0F per level */
+    /**
+     * Returns player's base damage (with sword) plus 1.0F per level
+     */
     private float getDamage(EntityPlayer player, ItemStack heldItemStack) {
-        float f = (float)player.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).getAttributeValue();
+        float f = (float) player.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).getAttributeValue();
         float f1 = EnchantmentHelper.getModifierForCreature(heldItemStack, EnumCreatureAttribute.UNDEFINED);
         return f + f1;
     }
 }
-
